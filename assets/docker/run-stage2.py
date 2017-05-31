2792bbd632b9d9a5eaef4ff487d1abd8463eab72
puneet kandhari <pkandhari@edgecast.com>
2017-05-31 14:22:49 -0700

    lets seee

---
+++ assets/docker/run-stage2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import sys
+import os
+if "DEBUG_TRUSTY" in os.environ:
+    print " env", repr(os.environ)
+    print "argv", repr(sys.argv)
+os.chdir(sys.argv[1])
+if "DEBUG_TRUSTY" in os.environ:
+    print "exec", repr(sys.argv[2:])
+os.execlp(sys.argv[2], *sys.argv[2:])
